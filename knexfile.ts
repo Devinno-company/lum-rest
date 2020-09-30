6b6b7279437f22db18e6f128a7497a695ed4f1f0
unknown <jg.oliveira.silv@gmail.com>
2020-09-30 10:51:00 -0300

    Lum API - v1.0.0: Iniciar banco de dados
    Configuração inicial do knex.

---
+++ knexfile.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+
+module.exports = {
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+    },
+    development: {
+        client: 'pg',
+        connection: {
+            host: 'localhost',
+            port: '5432',
+            user: 'postgres',
+            password: 'joaobanco',
+            database: 'db_lum'
+        },
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        }
+    },
+}
