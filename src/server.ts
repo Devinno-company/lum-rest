9ed2c20401ee982c7b6cad33b8badc1da8a21bc5
ArtGTom <artgtom@gmail.com>
2020-10-01 19:39:10 -0300

    Lum Rest: v1.0.1: Solve Hosting error.
    Solved an error in port management.

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import socketIo from 'socket.io';
 
 const server = http.createServer()
 const io = socketIo(server);
+const PORT = process.env.PORT || 3000;
+
 
 io.on('connection', socket => {
     io.emit('hello', 'DEU BOM');
     console.log('conectou 1');
 });
 
-server.listen(3000);
+server.listen(PORT);
