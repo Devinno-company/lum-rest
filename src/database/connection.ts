9ce4a067e1125a02988e87ca553b14410ac4f2cd
unknown <jg.oliveira.silv@gmail.com>
2020-11-27 13:35:07 -0300

    Lum Rest - v1.39.10: Add field profission in return of the team members

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,8 @@
 import knex from 'knex';
-const pg = require('pg');
+// const pg = require('pg');
 const { production } = require('../../knexfile');
 
-pg.defaults.ssl=true;
+// pg.defaults.ssl=true;
 const db = knex({
     client: 'pg',
     connection: production.connection,
